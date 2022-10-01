e48b8f5c860665a15d03f2aada097fd03c17ea46
mitrotasios <a.mitrotasios13@gmail.com>
2022-10-01 11:25:51 +0100

    minor fix

--- src/stories/chart-elements/LineChart.stories.tsx
+++ src/stories/chart-elements/LineChart.stories.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { Card, LineChart, Title } from 'components';
-import { data } from './testData';
+import { data } from './common/testData';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
