72e35810c75fadebb5a193ff6f08f0ad28342129
mitrotasios <a.mitrotasios13@gmail.com>
2022-10-02 22:35:38 +0100

    Update SimpleCard.tsx

--- src/stories/layout-elements/helpers/SimpleCard.tsx
+++ src/stories/layout-elements/helpers/SimpleCard.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-import { Card, Metric, Text } from 'components';
+import { ButtonInline, Card, Footer, Metric, Text } from 'components';
 
 export const SimpleCard = (args: any) => (
     <Card {...args}>
         <Metric>23,000</Metric>
         <Text>Sample Text</Text>
+        <Footer>
+            <ButtonInline text="View more" />
+        </Footer>
     </Card>
 );
