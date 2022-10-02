878ce72e24398fea09a936462757964022171ce7
mitrotasios <a.mitrotasios13@gmail.com>
2022-10-02 20:55:14 +0100

    fix: update Card stories

--- src/stories/layout-elements/Card.stories.tsx
+++ src/stories/layout-elements/Card.stories.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { AccordionList, Block, Card, ColGrid, Flex, Metric, Text, Title } from 'components';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Card from 'components/layout-elements/Card/Card';
+import { SimpleAccordion } from './helpers/SimpleAccordion';
+import { BaseColors, HorizontalPosition, VerticalPosition } from 'lib';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -10,21 +12,80 @@ export default {
 } as ComponentMeta<typeof Card>;
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 
-const Template: ComponentStory<typeof Card> = (args) => (
+const SimpleCard = (args: any) => (
     <Card {...args}>
-        <div>title</div>
+        <Metric>23,000</Metric>
+        <Text>Sample Text</Text>
     </Card>
 );
-  
-export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {
-    children: null
-};
 
-export const Decoration = Template.bind({});
+const ResponsiveTemplate: ComponentStory<typeof Card> = (args) => (
+    <>
+        <Title>Mobile</Title>
+        <div className="tr-w-64">
+            <SimpleCard { ...args } />
+        </div>
+        <Title marginTop="mt-5">Desktop</Title>
+        <SimpleCard { ...args } />
+    </>
+);
+
+const FlexTemplate: ComponentStory<typeof Card> = (args) => (
+    <>
+        <Flex alignItems="items-stretch" spaceX="space-x-2">
+            <SimpleCard { ...args } />
+            <SimpleCard { ...args } />
+            <Card { ...args }>
+                <Metric>$ 30,000</Metric>
+            </Card>
+        </Flex>
+        <Flex alignItems="items-stretch" marginTop="mt-5" spaceX="space-x-2">
+            <Block spaceY="space-y-2">
+                <SimpleCard { ...args } />
+                <SimpleCard { ...args } />
+                <SimpleCard { ...args } />
+            </Block>
+            <SimpleCard { ...args } />
+        </Flex>
+    </>
+);
+
+const DecorationPositionsTemplate: ComponentStory<typeof Card> = (args) => (
+    <ColGrid numCols={ 2 } gapX="gap-x-2" gapY="gap-y-2">
+        {   
+            ['left', 'top', 'right', 'bottom', 'mistyped'].map((
+                position,
+            ) => (
+                <Card { ...args } decoration={ position as (HorizontalPosition | VerticalPosition | '') }>
+                    <Title>{ `Decoration ${position}` }</Title>
+                </Card>
+            ))
+        }
+    </ColGrid>
+);
+
+const DecorationColorsTemplate: ComponentStory<typeof Card> = (args) => (
+    <ColGrid numCols={ 3 } gapX="gap-x-2" gapY="gap-y-2">
+        {   
+            Object.values(BaseColors).map((
+                color,
+            ) => (
+                <Card { ...args } decoration="top" decorationColor={ color }>
+                    <Title>{ `Decoration Color: ${color}` }</Title>
+                </Card>
+            ))
+        }
+    </ColGrid>
+);
+
+
+
+export const Default = ResponsiveTemplate.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Decoration.args = {
-    decoration: 'left',
-    children: null
-};
+
+export const DecorationPositions = DecorationPositionsTemplate.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const DecorationColors = DecorationColorsTemplate.bind({});
+
+export const WithFlexItemsStretch = FlexTemplate.bind({});
