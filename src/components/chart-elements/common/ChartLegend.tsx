37f94fc990b36938dafcdbe90c4bd1143098492d
mitrotasios <a.mitrotasios13@gmail.com>
2022-10-03 07:08:59 +0100

    fix: add windowsize listener to ChartLegend

--- src/components/chart-elements/common/ChartLegend.tsx
+++ src/components/chart-elements/common/ChartLegend.tsx
@@ -14,10 +14,19 @@ const ChartLegend = ({ payload }: any, colors: Color[] = themeColorRange,
 
     const legendRef = useRef<HTMLDivElement>(null);
     const [height, setHeight] = useState(calculateHeight(undefined));
+    
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_windowSize, setWindowSize] = useState(window.innerWidth);
     useEffect(() => {
+        const handleResize = () => setWindowSize(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+
         setLegendHeight(calculateHeight(legendRef.current?.clientHeight));
         setHeight(calculateHeight(height));
-    }, []);
+        
+        console.log(height);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [_windowSize]);
     return (
         <div ref={ legendRef } className="tr-flex tr-items-center tr-justify-end">
             <Legend
