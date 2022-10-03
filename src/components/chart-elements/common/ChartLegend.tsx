4c6d508c1d9e66a21f83661c1c51596f174fa778
mitrotasios <a.mitrotasios13@gmail.com>
2022-10-03 06:47:38 +0100

    fix: fix chart issues

--- src/components/chart-elements/common/ChartLegend.tsx
+++ src/components/chart-elements/common/ChartLegend.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { Color } from '../../../lib';
 import Legend from 'components/text-elements/Legend';
 import { themeColorRange } from 'lib';
 
-const ChartLegend = ({ payload }: any, colors: Color[] = themeColorRange) => {
+const ChartLegend = ({ payload }: any, colors: Color[] = themeColorRange,
+    setLegendHeight: React.Dispatch<React.SetStateAction<number>>) => {
+    const calculateHeight = (height: number|undefined) => (
+        height
+            ? Number(height) + 20 // 20px extra padding
+            : 60 // default height
+    );
+
+    const legendRef = useRef<HTMLDivElement>(null);
+    const [height, setHeight] = useState(calculateHeight(undefined));
+    useEffect(() => {
+        setLegendHeight(calculateHeight(legendRef.current?.clientHeight));
+        setHeight(calculateHeight(height));
+    }, []);
     return (
-        <div className="tr-flex tr-items-center tr-justify-end">
+        <div ref={ legendRef } className="tr-flex tr-items-center tr-justify-end">
             <Legend
                 categories={payload.map((entry: any) => entry.value)}
                 colors={ colors }
