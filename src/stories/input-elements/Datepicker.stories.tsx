cd60c873fa2e09f7ceb2550217bb7273f089dcc6
mitrotasios <a.mitrotasios13@gmail.com>
2022-11-06 17:11:07 +0000

    test: update stories

--- src/stories/input-elements/Datepicker.stories.tsx
+++ src/stories/input-elements/Datepicker.stories.tsx
@@ -86,10 +86,11 @@ const WithSelfDetectingModalTemplate: ComponentStory<typeof Datepicker> = () => 
     </ColGrid>
 );
 
-  
+const handleSelect = (startDate: Date, endDate: Date) => alert(`${startDate} + ${endDate}`);
+
 export const DefaultResponsive = ResponsiveTemplate.bind({});
 DefaultResponsive.args = {
-    handleSelect: (startDate, endDate) => alert(`${startDate} + ${endDate}`)
+    handleSelect: handleSelect,
 };
 
 export const WithFlexParent = FlexTemplate.bind({});
@@ -101,6 +102,7 @@ export const WithDefaultDates = ResponsiveTemplate.bind({});
 WithDefaultDates.args = {
     defaultStartDate: new Date(2022, 6, 1),
     defaultEndDate: new Date(2022, 7, 30),
+    handleSelect: handleSelect,
 };
 
 export const WithRelativeDatesDisabled = ResponsiveTemplate.bind({});
@@ -108,6 +110,7 @@ WithRelativeDatesDisabled.args = {
     defaultStartDate: new Date(2022, 6, 1),
     defaultEndDate: new Date(2022, 7, 30),
     enableRelativeDates: false,
+    handleSelect: handleSelect,
 };
 
 export const WithMinDate = ResponsiveTemplate.bind({});
@@ -124,6 +127,7 @@ export const WithMinAndMaxDate = ResponsiveTemplate.bind({});
 WithMinAndMaxDate.args = {
     minDate: new Date(2022, 6, 1),
     maxDate: new Date(2022, 7, 30),
+    handleSelect: handleSelect,
 };
 
 export const WithMinAndMaxDateAndDefaultDates = ResponsiveTemplate.bind({});
@@ -132,6 +136,7 @@ WithMinAndMaxDateAndDefaultDates.args = {
     defaultEndDate: new Date(2022, 7, 30),
     minDate: new Date(2022, 6, 1),
     maxDate: new Date(2022, 7, 30),
+    handleSelect: handleSelect,
 };
 
 export const WithDefaultDatesExceedingMinMax = ResponsiveTemplate.bind({});
@@ -157,4 +162,5 @@ export const WithSelfDetectingModal = WithSelfDetectingModalTemplate.bind({});
 export const WithDefaultRelativeFilterOption = ResponsiveTemplate.bind({});
 WithDefaultRelativeFilterOption.args = {
     defaultRelativeFilterOption: 'w',
+    handleSelect: handleSelect,
 };
