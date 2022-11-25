b80525b0744e846a98e72c932f45b1ffa413146c
Dmytro Radchuk <dmytro@tinybird.co>
2022-11-25 13:20:01 +0100

    Add href prop to BarList item Add BarList story with hrefs

--- src/stories/vis-elements/BarList.stories.tsx
+++ src/stories/vis-elements/BarList.stories.tsx
@@ -45,3 +45,17 @@ WithIcon.args = {
     ],
     valueFormatter: (value) => `${value} USD`
 };
+
+export const WithLinks = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+WithLinks.args = {
+    data: [
+        { name: '/home', value: 100000000, href: 'https://www.tremor.so/' },
+        { name: '/imprint', value: 351 },
+        { name: '/cancellation', value: 271, href: 'https://www.tremor.so/' },
+        { name: `/special-offer-august-getsahdkjhagskdfjhgakshjgdfkjahsgdfjkgasdjkhfgajkshgdfjkhagsdkjhfgajhksdgfjkhasdg
+            fjkhagsdjhkgfasjkdgfjkasdhgkjgfdsk`, value: 191, href: 'https://www.tremor.so/' },
+        { name: '/documentation', value: 0 },
+    ],
+    valueFormatter: (value) => `${value} USD`
+};
