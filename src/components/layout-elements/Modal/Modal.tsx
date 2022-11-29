8827f122ed4a96bef7adaeb76ceb37919a1c240f
mitrotasios <a.mitrotasios13@gmail.com>
2022-11-29 19:11:23 +0000

    fix: Modal height / posttion

--- src/components/layout-elements/Modal/Modal.tsx
+++ src/components/layout-elements/Modal/Modal.tsx
@@ -105,7 +105,7 @@ const Modal = ({
             <div
                 ref={ modalRef }
                 className={ classNames(
-                    'tr-absolute -tr-bottom-2 tr-translate-y-full tr-z-10 tr-divide-y tr-overflow-y-auto',
+                    'tr-absolute tr-z-10 tr-divide-y tr-overflow-y-auto',
                     width ? parseWidth(width) : 'tr-w-full',
                     getAbsoluteSpacing(),
                     maxHeight,
