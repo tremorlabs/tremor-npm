42e14aa55ce24eecafd0159da3c69b67b4f51560
Amjed B <amjed@winshot.net>
2023-02-01 12:43:02 +0100

    feat: add a storybook for datepicker i18n

--- src/stories/input-elements/DateRangePicker.stories.tsx
+++ src/stories/input-elements/DateRangePicker.stories.tsx
@@ -11,7 +11,7 @@ import {
   Title,
 } from "components";
 import { dateRangePickerData } from "stories/input-elements/helpers/testData";
-
+import { fr } from "date-fns/locale";
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Tremor/InputElements/DateRangePicker",
@@ -101,6 +101,16 @@ export const UncontrolledDefault = UncontrolledTemplate.bind({});
 export const UncontrolledWithDefaultDateRange = UncontrolledTemplate.bind({});
 UncontrolledWithDefaultDateRange.args = {
   defaultValue: [new Date(2022, 10, 1), new Date()],
+};
+export const UncontrolledWithDefaultFrLocale = UncontrolledTemplate.bind({});
+UncontrolledWithDefaultFrLocale.args = {
+  // defaultValue: [new Date(2022, 10, 1), new Date()],
+  locales: {
+    locale: fr,
+    weekDays: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+    pickerPlaceholder: "Sélectionnez",
+  },
+  placeholder: "Sélectionnez...",
 };
 
 export const UncontrolledWithDefaultSelectOption = UncontrolledTemplate.bind(
