9d49900a4b8fa15cf057688dd2ed011a5415731a
mitrotasios <a.mitrotasios13@gmail.com>
2023-03-11 15:45:44 +0000

    lint

--- src/stories/layout-elements/helpers/SimpleCard.tsx
+++ src/stories/layout-elements/helpers/SimpleCard.tsx
@@ -6,9 +6,7 @@ export const SimpleCard = (args: any) => (
   <Card {...args}>
     <Flex>
       <Metric>23,000</Metric>
-      <BadgeDelta deltaType="increase">
-        19.14%
-      </BadgeDelta>
+      <BadgeDelta deltaType="increase">19.14%</BadgeDelta>
     </Flex>
     <Text>Sample Text</Text>
   </Card>
