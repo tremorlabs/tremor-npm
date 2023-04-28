afdf7b841a1d6f42ca2976d479aa4e5c1c88a84c
bcarleton3 <bcarleton3@bloomberg.net>
2023-04-28 12:26:47 +0100

    remove node-modules from build + preserve modules

--- rollup.config.js
+++ rollup.config.js
@@ -3,43 +3,50 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser"
 
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
 import { typescriptPaths } from "rollup-plugin-typescript-paths";
 
-const packageJson = require("./package.json");
+const outputOptions = {
+  sourcemap: false,
+  preserveModules: true,
+  preserveModulesRoot: 'src',
+};
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        dir: "dist",
         format: "cjs",
-        sourcemap: false,
+        entryFileNames: "[name].cjs",
+        exports: "auto",
+        ...outputOptions,
       },
       {
-        file: packageJson.module,
+        dir: "dist",
         format: "esm",
-        sourcemap: false,
+        ...outputOptions,
       },
     ],
+    external: [/node_modules/],
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
+      terser(),
       typescript({
         tsconfig: "./tsconfig.json",
         exclude: ["**/stories/**", "**/tests/**", "./styles.css"],
       }),
-      terser(),
       typescriptPaths(),
     ],
   },
   {
-    input: "dist/esm/index.d.ts",
+    input: "dist/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
     external: [/\.css$/],
