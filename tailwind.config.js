8c8995b77fe7f8a53d867d808b7eb7bfb22a8014
Severin Landolt <sev.landolt@gmail.com>
2023-05-15 00:04:45 +0200

    tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,74 @@ module.exports = {
   theme: {
     transparent: "transparent",
     current: "currentColor",
-    "base-blue": "#1F7BE5",
-    "hover-blue": "#005CC6",
     extend: {
       colors: {
-        active: "#000fff",
+        active: "#000fff", //@achi?
+        tremor: {
+          brand: {
+            muted: "#bae6fd", // disabled
+            subtle: "#7dd3fc", // 300 zb hover
+            DEFAULT: "#3b82f6", // 500
+            emphasis: "#1d4ed8", // 700
+            inverted: "#eff6ff", //
+          },
+          background: {
+            muted: "#f9fafb", //canvasbackground 50
+            subtle: "#f3f4f6", // lightbackground 100
+            DEFAULT: "#ffffff", // background
+            emphasis: "#4b5563", // darkbackground
+            strong: "#1f2937", // darkestbackground
+            inverted: "#030712", // new
+          },
+          border: {
+            muted: "#f3f4f6", // new 100
+            subtle: "#e5e7eb", //lightBorder
+            DEFAULT: "#6b7280", //border
+            emphasis: "#374151", //darkborder
+          },
+          ring: {
+            subtle: "#e5e7eb", //lightring
+            DEFAULT: "#d1d5db", //ring
+          },
+          content: {
+            subtle: "#9ca3af", //lighttext
+            DEFAULT: "#6b7280", //text, //icon
+            emphasis: "#374151", //darktext
+            strong: "#1f2937", //darkesttext
+            inverted: "#ffffff", //new
+          },
+        },
+      },
+      boxShadow: {
+        tremor: {
+          shadow: {
+            sm: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+            DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+            md: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+            lg: "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+          },
+        },
+      },
+      borderRadius: {
+        tremor: {
+          rounded: {
+            sm: "0.125rem",
+            DEFAULT: "0.25rem",
+          },
+        },
+      },
+      fontSize: {
+        tremor: {
+          DEFAULT: ["1rem", { lineHeight: "1.5rem" }],
+        },
+      },
+      fontWeight: {
+        tremor: {
+          default: "400",
+          emphasis: "500",
+        },
       },
     },
+    plugins: [require("@headlessui/tailwindcss")],
   },
-  plugins: [require("@headlessui/tailwindcss")],
 };
