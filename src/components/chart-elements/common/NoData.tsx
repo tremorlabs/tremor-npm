8e1877ab4efc972dfe11a5675d8c957406ce8bfc
mitrotasios <a.mitrotasios13@gmail.com>
2023-05-27 10:40:19 +0300

    add dashed border

--- src/components/chart-elements/common/NoData.tsx
+++ src/components/chart-elements/common/NoData.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Flex, Text } from "components";
+import { twMerge } from "tailwind-merge";
+import { DEFAULT_COLOR, colorPalette, getColorClassNames } from "lib";
 
 interface NoDataProps {
   noDataText?: string;
 }
 const NoData = ({ noDataText = "No data" }: NoDataProps) => {
   return (
-    <Flex alignItems="center" justifyContent="center" className="w-full h-full">
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      className={twMerge(
+        "w-full h-full border border-dashed",
+        getColorClassNames(DEFAULT_COLOR, colorPalette.lightBorder).borderColor,
+      )}
+    >
       <Text>{noDataText}</Text>
     </Flex>
   );
