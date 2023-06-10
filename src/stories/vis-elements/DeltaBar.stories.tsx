05c3b35c8e692b35569dbc98c6b9759ca9e81f86
Severin Landolt <sev.landolt@gmail.com>
2023-06-10 01:36:19 -0700

    stories vis elements

--- src/stories/vis-elements/DeltaBar.stories.tsx
+++ src/stories/vis-elements/DeltaBar.stories.tsx
@@ -1,40 +1,61 @@
 import React from "react";
 
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Card, DeltaBar, Metric } from "components";
+import { Meta, StoryObj } from "@storybook/react";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
+import { Card, Metric, DeltaBar } from "components";
+
+const meta: Meta<typeof DeltaBar> = {
   title: "Tremor/VisElements/DeltaBar",
   component: DeltaBar,
-} as ComponentMeta<typeof DeltaBar>;
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-
-const Template: ComponentStory<typeof DeltaBar> = (args) => (
-  <Card>
-    <Metric>72.000 USD</Metric>
-    <DeltaBar {...args} />
-  </Card>
-);
-
-export const Increase = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Increase.args = {
-  value: 30,
-  className: "mt-5",
-  tooltip: "30%",
+  decorators: [(Story) => <Story />],
+  args: {
+    value: 50,
+    tooltip: "50%",
+    className: "mt-5",
+  },
 };
 
-export const Decrease = Template.bind({});
-Decrease.args = {
-  value: -50,
-  className: "mt-5",
-  tooltip: "-50%",
+export default meta;
+type Story = StoryObj<typeof DeltaBar>;
+
+const DeltaBarResponsiveFlexTemplate: Story = {
+  render: ({ ...args }) => {
+    return (
+      <Card>
+        <Metric>72.000 USD</Metric>
+        <DeltaBar {...args} />
+      </Card>
+    );
+  },
 };
 
-export const IncreaseReversed = Template.bind({});
-IncreaseReversed.args = {
-  value: 50,
-  isIncreasePositive: false,
-  className: "mt-5",
+export const DefaultExample: Story = {
+  ...DeltaBarResponsiveFlexTemplate,
+};
+
+export const IncreaseExample: Story = {
+  ...DeltaBarResponsiveFlexTemplate,
+  args: {
+    value: 30,
+    className: "mt-5",
+    tooltip: "30%",
+  },
+};
+
+export const DecreaseExample: Story = {
+  ...DeltaBarResponsiveFlexTemplate,
+  args: {
+    value: -50,
+    className: "mt-5",
+    tooltip: "-50%",
+  },
+};
+
+export const InversedExample: Story = {
+  ...DeltaBarResponsiveFlexTemplate,
+  args: {
+    value: 50,
+    isIncreasePositive: false,
+    className: "mt-5",
+  },
 };
--- src/stories/vis-elements/ProgressBar.stories.tsx
+++ src/stories/vis-elements/ProgressBar.stories.tsx
@@ -1,43 +1,49 @@
 import React from "react";
 
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 
-import { Card, Metric, ProgressBar } from "components";
+import { Card, Metric, ProgressBar, Flex } from "components";
 
 import { BaseColors } from "lib/constants";
-import { Flex } from "components";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
+const meta: Meta<typeof ProgressBar> = {
   title: "Tremor/VisElements/ProgressBar",
   component: ProgressBar,
-} as ComponentMeta<typeof ProgressBar>;
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+  decorators: [(Story) => <Story />],
+  args: {
+    value: 50,
+    tooltip: "50%",
+    className: "mt-5",
+    label: "90%",
+  },
+};
 
-const Template: ComponentStory<typeof ProgressBar> = (args) => (
-  <>
-    <Card className="mt-5">
-      <Metric>$23.456</Metric>
-      <Flex>
-        <ProgressBar {...args} />
-      </Flex>
-    </Card>
-    {Object.values(BaseColors).map((color) => (
-      <Card key={color} className="mt-5">
-        <Metric>$23.456</Metric>
-        <Flex>
-          <ProgressBar {...args} color={color} />
-        </Flex>
-      </Card>
-    ))}
-  </>
-);
+export default meta;
+type Story = StoryObj<typeof ProgressBar>;
 
-export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {
-  value: 50,
-  tooltip: "50%",
-  className: "mt-5",
-  label: "90%",
+const ProgressBarResponsiveFlexTemplate: Story = {
+  render: ({ ...args }) => {
+    return (
+      <>
+        <Card className="mt-5 max-w-md">
+          <Metric>$23.456</Metric>
+          <Flex>
+            <ProgressBar {...args} />
+          </Flex>
+        </Card>
+        {Object.values(BaseColors).map((color) => (
+          <Card key={color} className="mt-5 max-w-md">
+            <Metric>$23.456</Metric>
+            <Flex>
+              <ProgressBar {...args} color={color} />
+            </Flex>
+          </Card>
+        ))}
+      </>
+    );
+  },
+};
+
+export const DefaultExample: Story = {
+  ...ProgressBarResponsiveFlexTemplate,
 };
