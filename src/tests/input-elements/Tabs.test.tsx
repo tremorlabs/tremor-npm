63f5be61f50ec3319701eed447fe99a986e80030
Ed Cooper <ed.cooper@intelabsanalytics.com>
2023-08-09 13:09:18 +0100

    test: ARIA role and attr

--- src/tests/input-elements/Tabs.test.tsx
+++ src/tests/input-elements/Tabs.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Tab from "components/input-elements/Tabs/Tab";
 import TabList from "components/input-elements/Tabs/TabList";
@@ -23,5 +23,22 @@ describe("SelectBox", () => {
         </TabPanels>
       </TabGroup>,
     );
+  });
+  test("applies correct ARIA role and label to TabPanel", () => {
+    render(
+      <TabGroup>
+        <TabList>
+          <Tab>Option One</Tab>
+          <Tab>Option Two</Tab>
+          <Tab>Option Three</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>Hello World 1</TabPanel>
+          <TabPanel>Hello World 2</TabPanel>
+          <TabPanel>Hello World 3</TabPanel>
+        </TabPanels>
+      </TabGroup>,
+    );
+    expect(screen.getByRole("tabpanel", { name: "Option One" }));
   });
 });
