1449ca3647410d2d10d121909ae2068a78152b52
jzfrank <77217626+jzfrank@users.noreply.github.com>
2023-08-31 17:43:37 +0200

    add unit test for button

--- src/tests/input-elements/Button.test.tsx
+++ src/tests/input-elements/Button.test.tsx
@@ -1,11 +1,76 @@
 /* eslint-disable no-undef */
 import React from "react";
-import { render } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
+
+import userEvent from "@testing-library/user-event";
 
 import Button from "components/input-elements/Button/Button";
 
 describe("Button", () => {
   test("renders the Button component with default props", () => {
     render(<Button>Button</Button>);
+  });
+
+  test("renders the Button component with customized text", () => {
+    const buttonText = "Click Me";
+    const { getByText } = render(<Button>{buttonText}</Button>);
+
+    expect(getByText(buttonText)).toBeTruthy();
+  });
+
+  test("renders the Button component with customized onClick", () => {
+    const buttonText = "Click Me";
+    const onClick = jest.fn();
+    const { getByText } = render(<Button onClick={onClick}>{buttonText}</Button>);
+    expect(onClick).toHaveBeenCalledTimes(0);
+    fireEvent.click(getByText(buttonText));
+    expect(onClick).toHaveBeenCalledTimes(1);
+    fireEvent.click(getByText(buttonText));
+    expect(onClick).toHaveBeenCalledTimes(2);
+  });
+
+  test("renders the Button component with customized disabled", () => {
+    const buttonText = "Click Me";
+    const disabled = true;
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <Button disabled={disabled} onClick={onClick}>
+        {buttonText}
+      </Button>,
+    );
+
+    act(() => {
+      fireEvent.click(getByText(buttonText));
+    });
+
+    expect(onClick).toHaveBeenCalledTimes(0);
+  });
+
+  test("renders the Button component with customized icon", () => {
+    const buttonText = "Click Me";
+    const Icon = () => <div>Icon</div>;
+    const { queryByText } = render(
+      <Button icon={Icon} iconPosition="left">
+        {buttonText}
+      </Button>,
+    );
+
+    expect(queryByText("Icon")).toBeTruthy();
+  });
+
+  test("renders the Button component with tooltip", async () => {
+    const buttonText = "Click Me";
+    const tooltip = "some explanation";
+    const { getByText, debug } = render(<Button tooltip={tooltip}>{buttonText}</Button>);
+
+    await userEvent.hover(getByText(buttonText));
+    await waitFor(() => {
+      expect(screen.queryByText(tooltip)).toBeTruthy();
+    });
+
+    // mysteriously, below using fireEvent does not work
+    // fireEvent.mouseOver(getByText(buttonText));
+    // await screen.findByTitle(tooltip);
+    // expect(screen.queryByText(tooltip)).toBeTruthy();
   });
 });
