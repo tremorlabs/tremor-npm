7fd9851f727573c1974a1de9a0dc36cf7933cfea
Lucas Sequeira <lucas.sequeira@externos-cl.cencosud.com>
2023-09-14 01:10:53 -0300

    feat: add colspan and rowspan properties

--- src/components/list-elements/Table/TableHeaderCell.tsx
+++ src/components/list-elements/Table/TableHeaderCell.tsx
@@ -3,35 +3,41 @@ import { tremorTwMerge, makeClassName, spacing } from "lib";
 
 const makeTableHeaderCellClassName = makeClassName("TableHeaderCell");
 
-const TableHeaderCell = React.forwardRef<
-  HTMLTableCellElement,
-  React.HTMLAttributes<HTMLTableCellElement>
->((props, ref) => {
-  const { children, className, ...other } = props;
-  return (
-    <>
-      <th
-        ref={ref}
-        className={tremorTwMerge(
-          makeTableHeaderCellClassName("root"),
-          // common
-          "sticky whitespace-nowrap text-left font-semibold",
-          // light
-          "text-tremor-content",
-          // dark
-          "dark:text-dark-tremor-content",
-          spacing.none.top,
-          spacing.twoXl.paddingX,
-          spacing.xl.paddingY,
-          className,
-        )}
-        {...other}
-      >
-        {children}
-      </th>
-    </>
-  );
-});
+export interface TableHeaderCellProps extends React.HTMLAttributes<HTMLTableCellElement> {
+  colSpan?: number;
+  rowSpan?: number;
+}
+
+const TableHeaderCell = React.forwardRef<HTMLTableCellElement, TableHeaderCellProps>(
+  (props, ref) => {
+    const { children, className, colSpan, rowSpan, ...other } = props;
+    return (
+      <>
+        <th
+          ref={ref}
+          className={tremorTwMerge(
+            makeTableHeaderCellClassName("root"),
+            // common
+            "sticky whitespace-nowrap text-left font-semibold",
+            // light
+            "text-tremor-content",
+            // dark
+            "dark:text-dark-tremor-content",
+            spacing.none.top,
+            spacing.twoXl.paddingX,
+            spacing.xl.paddingY,
+            className,
+          )}
+          colSpan={colSpan}
+          rowSpan={rowSpan}
+          {...other}
+        >
+          {children}
+        </th>
+      </>
+    );
+  },
+);
 
 TableHeaderCell.displayName = "TableHeaderCell";
 
