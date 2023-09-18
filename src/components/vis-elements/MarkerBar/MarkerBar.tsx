65e328b3362b2937c900fd4311db636ff3611fbb
mbauchet <90607026+mbauchet@users.noreply.github.com>
2023-09-18 23:56:13 +0200

    fix: MarkerBar short circuit (#695)

--- src/components/vis-elements/MarkerBar/MarkerBar.tsx
+++ src/components/vis-elements/MarkerBar/MarkerBar.tsx
@@ -53,7 +53,7 @@ const MarkerBar = React.forwardRef<HTMLDivElement, MarkerBarProps>((props, ref) 
       )}
       {...other}
     >
-      {minValue && maxValue && (
+      {minValue !== undefined && maxValue !== undefined ? (
         <>
           <Tooltip text={rangeTooltip} {...rangeTooltipProps} />
           <div
@@ -75,7 +75,7 @@ const MarkerBar = React.forwardRef<HTMLDivElement, MarkerBarProps>((props, ref) 
             {...getRangeReferenceProps}
           />
         </>
-      )}
+      ) : null}
       <Tooltip text={markerTooltip} {...markerTooltipProps} />
       <div
         ref={markerTooltipProps.refs.setReference}
