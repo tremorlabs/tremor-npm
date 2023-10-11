821d3e5bdc26e6da1e3413f627d63001e951632c
Severin Landolt <sev.landolt@gmail.com>
2023-10-11 12:59:29 +0200

    search select stories

--- src/stories/input-elements/helpers/SimpleSearchSelect.tsx
+++ src/stories/input-elements/helpers/SimpleSearchSelect.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { SearchSelect, SearchSelectItem } from "components";
+import { Button, SearchSelect, SearchSelectItem } from "components";
 import { CalendarIcon } from "assets";
 
 const MyIcon = CalendarIcon;
@@ -16,3 +16,28 @@ export const SimpleSearchSelect = (args: any) => (
     </SearchSelectItem>
   </SearchSelect>
 );
+
+export const SimpleSearchSelectControlled = (args: any) => {
+  const [value, setValue] = React.useState<string>("5");
+  return (
+    <div className="space-y-4">
+      <SearchSelect
+        value={value}
+        onValueChange={(value) => {
+          setValue(value);
+          alert(value);
+        }}
+        {...args}
+      >
+        <SearchSelectItem value={"5"}>Five</SearchSelectItem>
+        <SearchSelectItem value={"3"}>Three</SearchSelectItem>
+        <SearchSelectItem value={"1"}>One</SearchSelectItem>
+        <SearchSelectItem value={"30"}>Thirty</SearchSelectItem>
+        <SearchSelectItem value={"33"}>Thirtythree</SearchSelectItem>
+      </SearchSelect>
+      <Button onClick={() => setValue("")}>Reset</Button>
+      <Button onClick={() => setValue("1")}>One</Button>
+      <p className="text-slate-500">{value}</p>
+    </div>
+  );
+};
