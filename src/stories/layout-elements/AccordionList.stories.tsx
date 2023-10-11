10b25a5aaa0335240bb79c8a334b3133bc9d0f8f
Severin Landolt <sev.landolt@gmail.com>
2023-10-11 20:19:32 +0200

    accordion stories

--- src/stories/layout-elements/AccordionList.stories.tsx
+++ src/stories/layout-elements/AccordionList.stories.tsx
@@ -2,82 +2,31 @@ import React from "react";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { AccordionList, Card, Flex, Text, Title } from "components";
-import { SimpleAccordion } from "./helpers/SimpleAccordion";
+import { AccordionList } from "components";
+import { SimpleAccordion, SimpleAccordionList } from "./helpers/SimpleAccordion";
 
-
-export default {
+const meta: Meta<typeof AccordionList> = {
   title: "Tremor/LayoutElements/AccordionList",
   component: AccordionList,
-} as ComponentMeta<typeof AccordionList>;
-
-
-const SimpleAccordionList = ({ ...args }) => {
-  return (
-    <AccordionList {...args}>
-      <SimpleAccordion />
-      <SimpleAccordion />
-      <SimpleAccordion />
-    </AccordionList>
-  );
 };
 
-const ResponsiveTemplate: ComponentStory<typeof AccordionList> = (args) => (
-  <>
-    <Title>Mobile</Title>
-    <div className="w-64">
-      <Card>
-        <SimpleAccordionList {...args} length={length} />
-      </Card>
-    </div>
-    <Title className="mt-5">Desktop</Title>
-    <Card>
-      <SimpleAccordionList {...args} length={length} />
-    </Card>
-  </>
-);
+export default meta;
+type Story = StoryObj<typeof AccordionList>;
 
-const FlexTemplate: ComponentStory<typeof AccordionList> = (args) => (
-  <Card>
-    <Text className="mt-2">Justify Start</Text>
-    <Flex justifyContent="start" className="mt-2">
-      <SimpleAccordionList {...args} />
-    </Flex>
-    <Text className="mt-2">Justify End</Text>
-    <Flex justifyContent="end" className="mt-2">
-      <SimpleAccordionList {...args} />
-    </Flex>
-    <Text className="mt-2">Justify End with inner div</Text>
-    <Flex justifyContent="end" className="mt-2">
-      <div>
-        <SimpleAccordionList {...args} />
-      </div>
-    </Flex>
-    <Text className="mt-2">Justify Start with inner div</Text>
-    <Flex justifyContent="start" className="mt-2">
-      <div>
-        <SimpleAccordionList {...args} />
-      </div>
-    </Flex>
-  </Card>
-);
+export const Default: Story = {
+  render: SimpleAccordionList,
+};
 
-const LessThanThreeChildrenTemplate: ComponentStory<typeof AccordionList> = (args) => (
-  <Card>
-    <AccordionList {...args}>
-      <SimpleAccordion />
-    </AccordionList>
-    <AccordionList className="mt-5">
-      <SimpleAccordion />
-      <SimpleAccordion />
-    </AccordionList>
-  </Card>
-);
-
-export const DefaultResponsive = ResponsiveTemplate.bind({});
-
-export const WithFlexParent = FlexTemplate.bind({});
-
-export const WithNoShadow = ResponsiveTemplate.bind({});
-
-export const WithLessThanThreeChildren = LessThanThreeChildrenTemplate.bind({});
+export const WithLessThanThreeChildren: Story = {
+  render: (args) => (
+    <>
+      <AccordionList {...args}>
+        <SimpleAccordion />
+      </AccordionList>
+      <AccordionList className="mt-5">
+        <SimpleAccordion />
+        <SimpleAccordion />
+      </AccordionList>
+    </>
+  ),
+};
