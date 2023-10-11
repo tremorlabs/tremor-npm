5beea01028a15c52a73d0da4d54b97f4512f0e15
Severin Landolt <sev.landolt@gmail.com>
2023-10-11 21:28:38 +0200

    Update Subtitle.stories.tsx

--- src/stories/text-elements/Subtitle.stories.tsx
+++ src/stories/text-elements/Subtitle.stories.tsx
@@ -1,37 +1,27 @@
-import React from "react";
-
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Card } from "components";
 import Subtitle from "components/text-elements/Subtitle/Subtitle";
 
-
-export default {
+const meta: Meta<typeof Subtitle> = {
   title: "Tremor/TextElements/Subtitle",
   component: Subtitle,
-} as ComponentMeta<typeof Subtitle>;
-
-
-const Template: ComponentStory<typeof Subtitle> = (args) => (
-  <Card>
-    <Subtitle {...args} className="truncate">
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-      been the industry&apos;s standard dummy text ever since the 900s, when an unknown printer took
-      a galley of type andSubtitleambled it to make a type specimen book.
-    </Subtitle>
-  </Card>
-);
-
-export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {
-  children: null,
 };
 
-export const TextColor = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-TextColor.args = {
-  color: "green",
-  className: "text-left",
-  children: null,
+export default meta;
+type Story = StoryObj<typeof Subtitle>;
+
+export const Default: Story = {
+  args: {
+    children:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempor lorem non est congue blandit. Praesent non lorem sodales, suscipit est sed, hendrerit dolor.",
+  },
+};
+
+export const Color: Story = {
+  args: {
+    children:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempor lorem non est congue blandit. Praesent non lorem sodales, suscipit est sed, hendrerit dolor.",
+    color: "green",
+    className: "text-left",
+  },
 };
