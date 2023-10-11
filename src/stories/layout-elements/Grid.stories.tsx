778dff6d860477b1131d037d515aab826a224f32
Severin Landolt <sev.landolt@gmail.com>
2023-10-11 15:11:30 +0200

    Update Grid.stories.tsx

--- src/stories/layout-elements/Grid.stories.tsx
+++ src/stories/layout-elements/Grid.stories.tsx
@@ -6,31 +6,28 @@ import { Card, Col, Grid, Metric } from "components";
 import { SimpleCard } from "./helpers/SimpleCard";
 import { SimpleText } from "./helpers/SimpleText";
 
-
-export default {
-  title: "Tremor/LayoutElements/Grid",
+const meta: Meta<typeof Grid> = {
+  title: "Tremor/InputElements/Grid",
   component: Grid,
-} as ComponentMeta<typeof Grid>;
+};
 
+export default meta;
+type Story = StoryObj<typeof Grid>;
 
-const Template: ComponentStory<typeof Grid> = (args) => (
-  <Grid {...args}>
-    <Col numColSpanMd={2}>
+export const ThreeItems: Story = {
+  render: ({ ...args }) => (
+    <Grid {...args}>
+      <Col numColSpanMd={2}>
+        <SimpleCard />
+      </Col>
       <SimpleCard />
-    </Col>
-    <SimpleCard />
-    <SimpleCard />
-    <SimpleCard />
-    <Card>
-      <Metric>$ 40,000</Metric>
-      <SimpleText />
-    </Card>
-  </Grid>
-);
-
-export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {
-  numItemsMd: 3,
-  className: "gap-x-2 gap-y-2",
+      <SimpleCard />
+      <SimpleCard />
+      <Card>
+        <Metric>$ 40,000</Metric>
+        <SimpleText />
+      </Card>
+    </Grid>
+  ),
+  args: { numItemsMd: 3, className: "gap-x-2 gap-y-2" },
 };
