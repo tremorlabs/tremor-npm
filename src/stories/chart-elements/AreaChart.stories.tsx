3854b91782e0bc8d3edaf457060a612eb5f0547d
Severin Landolt <sev.landolt@gmail.com>
2023-10-11 23:11:52 +0200

    Update AreaChart.stories.tsx

--- src/stories/chart-elements/AreaChart.stories.tsx
+++ src/stories/chart-elements/AreaChart.stories.tsx
@@ -127,26 +127,26 @@ export const SingleAndMultipleDataAndOnValueChange: Story = {
   args: { data: singleAndMultipleData, onValueChange: (v: any) => alert(JSON.stringify(v)) },
 };
 
-export const WithMultipleZeroValues = ResponsiveTemplate.bind({});
-WithMultipleZeroValues.args = {
-  ...args,
-  data: [
-    {
-      month: "May 21",
-      Sales: 2390,
-      "Successful Payments": 0,
-    },
-    {
-      month: "Jun 21'",
-      Sales: 2390,
-      "Successful Payments": 0,
-    },
-    {
-      month: "Jul 21'",
-      Sales: 3490,
-      "Successful Payments": 0,
-    },
-  ],
+export const MultipleZeroValues: Story = {
+  args: {
+    data: [
+      {
+        month: "May 21",
+        Sales: 2390,
+        "Successful Payments": 0,
+      },
+      {
+        month: "Jun 21'",
+        Sales: 2390,
+        "Successful Payments": 0,
+      },
+      {
+        month: "Jul 21'",
+        Sales: 3490,
+        "Successful Payments": 0,
+      },
+    ],
+  },
 };
 
 //Custom tooltips
