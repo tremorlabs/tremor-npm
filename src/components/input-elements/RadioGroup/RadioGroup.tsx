b4c1252cc125f159de229b7f84517dcb80dfcf48
Jin <frankjinzhang@gmail.com>
2023-11-12 21:50:19 +0100

    set up RadioGroup and RadioGroupButton

--- src/components/input-elements/RadioGroup/RadioGroup.tsx
+++ src/components/input-elements/RadioGroup/RadioGroup.tsx
@@ -1,35 +1,28 @@
 "use client";
 import { RadioGroup as HeadlessRadioGroup } from "@headlessui/react";
-import { makeClassName, tremorTwMerge } from "lib";
+import { tremorTwMerge } from "lib";
 import React from "react";
 
-const makeRadioGroupClassName = makeClassName("RadioGroup");
-
 export interface RadioGroupProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "onChange"> {
-  defaultValue?: string[];
-  value?: string[];
-  onChange?: (value: any) => void; // @SEV: does this need to be declared here? see e.g. BaseInput
-  disabled?: boolean;
-  name?: string;
-  id?: string;
+  defaultValue?: string;
+  value?: string;
+  onValueChange?: (value: string) => void;
 }
 
 const RadioGroup = React.forwardRef<HTMLDivElement, RadioGroupProps>((props, ref) => {
-  const { defaultValue, value, onChange, children, disabled, name, id, className, ...other } =
-    props;
+  const { defaultValue, value, onValueChange, className, children } = props;
 
   return (
     <HeadlessRadioGroup
       as="div"
-      ref={ref}
       defaultValue={defaultValue}
+      ref={ref}
       value={value}
-      onChange={onChange}
-      disabled={disabled}
-      name={name}
-      id={id}
-      className={tremorTwMerge(makeRadioGroupClassName("root"), className)}
-      {...other}
+      onChange={(e) => {
+        console.log(e);
+        onValueChange?.(e);
+      }}
+      className={tremorTwMerge("flex gap-2", className)}
     >
       {children}
     </HeadlessRadioGroup>
--- src/stories/input-elements/RadioGroup.stories.tsx
+++ src/stories/input-elements/RadioGroup.stories.tsx
@@ -1,24 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
-// import { RadioGroup, RadioGroupOption } from "components";
-import { RadioGroup } from "@headlessui/react";
-import { CheckCircleIcon } from "@heroicons/react/solid";
 import React, { useState } from "react";
+import { RadioGroup, RadioGroupOption } from "components";
 
 const platformPrices = [
-  { id: 0, title: "Starter", description: "Up to 300 requests per day" },
+  { id: "0", title: "Starter", description: "Up to 300 requests per day", price: 0 },
   {
-    id: 2,
+    id: "2",
     title: "Premium",
     description: "Unlimited requests per month",
+    price: 100,
   },
-  { id: 3, title: "Enterprise", description: "Based on your specific needs" },
+  { id: "3", title: "Enterprise", description: "Based on your specific needs", price: 1000 },
 ];
-
-interface StringJoiner {
-  (...classes: string[]): string;
-}
-const classNames: StringJoiner = (...classes: string[]): string =>
-  classes.filter(Boolean).join(" ");
 
 const meta: Meta<typeof RadioGroup> = {
   title: "Components/Input/RadioGroup",
@@ -32,105 +25,20 @@ function CardTemplate() {
   const [selectedPlatform, setSelectedPlatform] = useState(platformPrices[0]);
   return (
     <div className="p-10">
-      {/* <RadioGroup value={selectedPlatform} onChange={setSelectedPlatform} name="platform">
-        <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
-          {platformPrices.map((priceTier) => (
-            <RadioGroupOption
-              key={priceTier.id}
-              value={priceTier.id}
-              className={({ active }) =>
-                classNames(
-                  active
-                    ? "border border-blue-400 ring-2 ring-blue-200 transition-all"
-                    : "ring-1 ring-gray-200 border-transparent",
-                  "relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm transition-all",
-                )
-              }
-            >
-              {({ checked, active }) => (
-                <>
-                  <span className="flex flex-1">
-                    <span className="flex flex-col">
-                      <span className="block text-sm font-medium text-slate-900">
-                        {priceTier.title}
-                      </span>
-                      <span className="mt-1 flex items-center text-sm text-slate-500">
-                        {priceTier.description}
-                      </span>
-                    </span>
-                  </span>
-                  <CheckCircleIcon
-                    className={classNames(!checked ? "invisible" : "", "h-5 w-5 text-blue-500")}
-                    aria-hidden="true"
-                  />
-                  <span
-                    className={classNames(
-                      active ? "border" : "",
-                      checked
-                        ? "border border-blue-400 ring-2 ring-blue-200"
-                        : "border-transparent",
-                      "pointer-events-none absolute -inset-px rounded-lg",
-                    )}
-                    aria-hidden="true"
-                  />
-                </>
-              )}
-            </RadioGroupOption>
-          ))}
-        </div>
-      </RadioGroup> */}
-      <RadioGroup value={selectedPlatform} onChange={setSelectedPlatform} name="platform">
-        <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
-          {platformPrices.map((priceTier) => (
-            <RadioGroup.Option
-              key={priceTier.id}
-              value={priceTier}
-              className={({ active }) =>
-                classNames(
-                  active
-                    ? "border border-blue-400 ring-2 ring-blue-200 transition-all"
-                    : "ring-1 ring-gray-200 border-transparent ",
-                  "relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm transition-all",
-                )
-              }
-            >
-              {({ checked, active }) => (
-                <>
-                  <span className="flex flex-1">
-                    <span className="flex flex-col">
-                      <RadioGroup.Label
-                        as="span"
-                        className="block text-sm font-medium text-slate-900"
-                      >
-                        {priceTier.title}
-                      </RadioGroup.Label>
-                      <RadioGroup.Description
-                        as="span"
-                        className="mt-1 flex items-center text-sm text-slate-500"
-                      >
-                        {priceTier.description}
-                      </RadioGroup.Description>
-                    </span>
-                  </span>
-                  <CheckCircleIcon
-                    className={classNames(!checked ? "invisible" : "", "h-5 w-5 text-blue-500")}
-                    aria-hidden="true"
-                  />
-                  <span
-                    className={classNames(
-                      active ? "border" : "",
-                      checked
-                        ? "border border-blue-400 ring-2 ring-blue-200"
-                        : "border-transparent",
-                      "pointer-events-none absolute -inset-px rounded-lg",
-                    )}
-                    aria-hidden="true"
-                  />
-                </>
-              )}
-            </RadioGroup.Option>
-          ))}
-        </div>
+      <RadioGroup
+        value={selectedPlatform.id}
+        onValueChange={(val) =>
+          setSelectedPlatform((prev) => platformPrices.find((ele) => ele.id === val) ?? prev)
+        }
+      >
+        {platformPrices.map((priceTier) => (
+          <RadioGroupOption
+            key={priceTier.id}
+            value={priceTier.id}
+            label={priceTier.title}
+            description={priceTier.description}
+          />
+        ))}
       </RadioGroup>
     </div>
   );
@@ -140,6 +48,6 @@ const Cards: Story = {
   render: CardTemplate,
 };
 
-export const CardsCustomStyled: Story = {
+export const DefaultRadioGroup: Story = {
   ...Cards,
 };
