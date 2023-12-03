7bf19c288d5ae3872a3711ce9c60e6fb381c40fc
Severin Landolt <sev.landolt@gmail.com>
2023-12-03 20:12:49 +0100

    Update Tracker.stories.tsx

--- src/stories/vis-elements/Tracker.stories.tsx
+++ src/stories/vis-elements/Tracker.stories.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-import { CheckCircleIcon } from "@heroicons/react/solid";
-
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Card, Tracker } from "components";
+import { TrackerBlockProps } from "components/vis-elements/Tracker/Tracker";
 
-const data = [
+const data: TrackerBlockProps[] = [
   { color: "emerald", tooltip: "Tracker Info" },
   { color: "emerald", tooltip: "Tracker Info" },
   { color: "emerald", tooltip: "Tracker Info" },
@@ -26,7 +25,6 @@ const data = [
   { color: "yellow", tooltip: "Tracker Info" },
   { color: "emerald", tooltip: "Tracker Info" },
   { color: "emerald", tooltip: "Tracker Info" },
-  { tooltip: "Tracker Info" },
   { color: "emerald", tooltip: "Tracker Info" },
   { color: "emerald", tooltip: "Tracker Info" },
   { color: "emerald", tooltip: "Tracker Info" },
@@ -115,48 +113,12 @@ const meta: Meta<typeof Tracker> = {
 export default meta;
 type Story = StoryObj<typeof Tracker>;
 
-// export const Default: Story = {};
-
-export const Default: Story = {
-  render: (args) => (
-    <Card>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-2">
-          <CheckCircleIcon className="h-5 w-5 text-emerald-500" aria-hidden="true" />
-          <p className="text-tremor-default text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-medium">
-            example.com
-          </p>
-        </div>
-        <p className="text-tremor-default text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-medium">
-          98.1% uptime
-        </p>
-      </div>
-      <Tracker {...args} className="mt-3" />
-      <p className="mt-4 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-        Last 90 days
-      </p>
-    </Card>
-  ),
-};
+export const Default: Story = {};
 
 export const MaxWidthMd: Story = {
   render: () => (
     <Card className="max-w-md">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-2">
-          <CheckCircleIcon className="h-5 w-5 text-emerald-500" aria-hidden="true" />
-          <p className="text-tremor-default text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-medium">
-            example.com
-          </p>
-        </div>
-        <p className="text-tremor-default text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-medium">
-          98.1% uptime
-        </p>
-      </div>
       <Tracker data={data.slice(60, 90)} className="mt-3" />
-      <p className="mt-3 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-        Last 30 days
-      </p>
     </Card>
   ),
 };
@@ -164,21 +126,7 @@ export const MaxWidthMd: Story = {
 export const MaxWidthXs: Story = {
   render: () => (
     <Card className="max-w-xs">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-2">
-          <CheckCircleIcon className="h-5 w-5 text-emerald-500" aria-hidden="true" />
-          <p className="text-tremor-default text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-medium">
-            example.com
-          </p>
-        </div>
-        <p className="text-tremor-default text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-medium">
-          98.1% uptime
-        </p>
-      </div>
       <Tracker data={data.slice(60, 90)} className="mt-3" />
-      <p className="mt-3 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-        Last 30 days
-      </p>
     </Card>
   ),
 };
