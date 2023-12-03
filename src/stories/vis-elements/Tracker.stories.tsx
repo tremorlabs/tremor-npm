34b47ad77a2ff9793fc199d0dad724362e1a9b63
Severin Landolt <41927988+severinlandolt@users.noreply.github.com>
2023-12-03 20:16:43 +0100

    chore: tracker facelift (#839)
    * add: tracker facelift
    
    * Update Tracker.stories.tsx
    
    ---------
    
    Co-authored-by: christopherkindl <53372002+christopherkindl@users.noreply.github.com>

--- src/stories/vis-elements/Tracker.stories.tsx
+++ src/stories/vis-elements/Tracker.stories.tsx
@@ -2,36 +2,106 @@ import React from "react";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Tracker } from "components";
+import { Card, Tracker } from "components";
+import { TrackerBlockProps } from "components/vis-elements/Tracker/Tracker";
+
+const data: TrackerBlockProps[] = [
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "red", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "red", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "yellow", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "yellow", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "red", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+  { color: "emerald", tooltip: "Tracker Info" },
+];
 
 const meta: Meta<typeof Tracker> = {
   title: "Visualizations/Vis/Tracker",
   component: Tracker,
   args: {
-    data: [
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "yellow", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "red", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "yellow", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "red", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "yellow", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-      { color: "emerald", tooltip: "Tracker Info" },
-    ],
+    data: data,
   },
   render: (args) => <Tracker {...args} />,
   parameters: {
@@ -46,9 +116,17 @@ type Story = StoryObj<typeof Tracker>;
 export const Default: Story = {};
 
 export const MaxWidthMd: Story = {
-  args: { className: "max-w-md" },
+  render: () => (
+    <Card className="max-w-md">
+      <Tracker data={data.slice(60, 90)} className="mt-3" />
+    </Card>
+  ),
 };
 
 export const MaxWidthXs: Story = {
-  args: { className: "max-w-xs" },
+  render: () => (
+    <Card className="max-w-xs">
+      <Tracker data={data.slice(60, 90)} className="mt-3" />
+    </Card>
+  ),
 };
