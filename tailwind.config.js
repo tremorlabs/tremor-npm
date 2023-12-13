9513b803d5f15baa9b813d676cc56b96f073f0e8
severinlandolt <sev.landolt@gmail.com>
2023-12-13 17:18:57 +0100

    fix: config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -122,7 +122,7 @@ module.exports = {
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
 
-    // custom colors
+    // custom colors charts
     ...["[#32a852]", "[#fcba03]"].flatMap((customColor) => [
       `bg-${customColor}`,
       `border-${customColor}`,
