1d4fed7ff476fb19a84c26ca8d0159c1784136d3
Touha Zohair <touha98@gmail.com>
2023-12-31 21:45:12 +0600

    separate root element

--- src/components/layout-elements/Dialog/Dialog.tsx
+++ src/components/layout-elements/Dialog/Dialog.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { border, makeClassName, tremorTwMerge } from "lib";
+import { makeClassName, tremorTwMerge } from "lib";
 import { Dialog as HeadlessuiDialog } from "@headlessui/react";
-import { RootStylesContext } from "contexts";
 
 const makeDisplayClassName = makeClassName("Dialog");
 
@@ -13,56 +12,35 @@ export type DialogProps = React.HTMLAttributes<HTMLDivElement> & {
   onClose: (val: boolean) => void;
 } & XOR<{ unmount?: boolean }, { static?: boolean }>;
 
-const Dialog = React.forwardRef<HTMLDivElement, DialogProps>(
-  (
-    props = {
-      open: false,
-      onClose: () => {},
-      unmount: true,
-    },
-    ref,
-  ) => {
-    const { children, className, ...other } = props;
-    const rootStyles =
-      React.useContext(RootStylesContext) ?? tremorTwMerge(border.sm.all, "rounded-tremor-default");
+const Dialog = React.forwardRef<HTMLDivElement, DialogProps>((props, ref) => {
+  const { children, className, ...other } = props;
 
-    return (
-      <HeadlessuiDialog
-        as="div"
-        ref={ref}
-        {...other}
-        className={tremorTwMerge(
-          makeDisplayClassName("root"),
-          //light
-          "bg-tremor-background border-tremor-border",
-          // dark
-          "dark:bg-dark-tremor-background dark:border-dark-tremor-border",
-          // dialog
-          "relative z-50",
-          className,
-        )}
-      >
-        <div className="fixed inset-0 bg-dark-tremor-background/20 dark:bg-tremor-background/20 opacity-50"></div>
+  return (
+    <HeadlessuiDialog
+      as="div"
+      ref={ref}
+      {...other}
+      className={tremorTwMerge(
+        makeDisplayClassName("root"),
+        //light
+        "bg-tremor-background border-tremor-border",
+        // dark
+        "dark:bg-dark-tremor-background dark:border-dark-tremor-border",
+        // dialog
+        "relative z-50",
+        className,
+      )}
+    >
+      <div className="fixed inset-0 bg-dark-tremor-background/20 dark:bg-tremor-background/20 opacity-50"></div>
 
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <HeadlessuiDialog.Panel
-              className={tremorTwMerge(
-                "w-full max-w-xl transform overflow-hidden",
-                "bg-tremor-background border-tremor-border",
-                "dark:bg-dark-tremor-background dark:border-dark-tremor-border",
-                "p-6 text-left align-middle shadow-tremor transition-all rounded-xl",
-                rootStyles,
-              )}
-            >
-              {children}
-            </HeadlessuiDialog.Panel>
-          </div>
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4 text-center">
+          {children}
         </div>
-      </HeadlessuiDialog>
-    );
-  },
-);
+      </div>
+    </HeadlessuiDialog>
+  );
+});
 
 Dialog.displayName = "Dialog";
 
