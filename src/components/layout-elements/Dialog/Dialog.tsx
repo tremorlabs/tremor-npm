188449f3b47b8dfe68b0d31c7ec8a68e168fc668
Touha Zohair <39002455+touha98@users.noreply.github.com>
2024-01-06 03:50:07 +0600

    feat: Dialog Component (#886)
    * basic dialog component
    
    ---------
    
    Co-authored-by: severinlandolt <sev.landolt@gmail.com>
    Co-authored-by: christopherkindl <53372002+christopherkindl@users.noreply.github.com>

---
+++ src/components/layout-elements/Dialog/Dialog.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Dialog as HeadlessuiDialog, Transition } from "@headlessui/react";
+import { makeClassName, tremorTwMerge } from "lib";
+
+const makeDisplayClassName = makeClassName("dialog");
+
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
+
+export type DialogProps = React.HTMLAttributes<HTMLDivElement> & {
+  open: boolean;
+  onClose: (val: boolean) => void;
+  overlayClassName?: string;
+} & XOR<{ unmount?: boolean }, { static?: boolean }>;
+
+const Dialog = React.forwardRef<HTMLDivElement, DialogProps>((props, ref) => {
+  const { children, className, overlayClassName, ...other } = props;
+
+  return (
+    <Transition as={React.Fragment} appear show={props.open}>
+      <HeadlessuiDialog
+        as="div"
+        ref={ref}
+        {...other}
+        className={tremorTwMerge(makeDisplayClassName("root"), "relative z-50", className)}
+      >
+        <Transition.Child
+          as={React.Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            className={tremorTwMerge(
+              "fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity",
+              overlayClassName,
+            )}
+          ></div>
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">{children}</div>
+        </div>
+      </HeadlessuiDialog>
+    </Transition>
+  );
+});
+
+Dialog.displayName = "Dialog";
+
+export default Dialog;
