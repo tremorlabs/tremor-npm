939f635179a9e146845351215ac966059845c068
Severin Landolt <41927988+severinlandolt@users.noreply.github.com>
2024-01-07 23:57:27 +0100

    feat: dialog (#895)
    * feat: Dialog Component (#886)
    
    * basic dialog component
    
    ---------
    
    Co-authored-by: severinlandolt <sev.landolt@gmail.com>
    Co-authored-by: christopherkindl <53372002+christopherkindl@users.noreply.github.com>
    
    * Update README.md
    
    * Update dialog
    
    * fix: dialog story
    
    * fix: add max-w default to panel
    
    * fix: classify background as theme token
    
    ---------
    
    Co-authored-by: Touha Zohair <39002455+touha98@users.noreply.github.com>
    Co-authored-by: christopherkindl <53372002+christopherkindl@users.noreply.github.com>

---
+++ src/components/layout-elements/Dialog/DialogPanel.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Dialog as HeadlessuiDialog, Transition } from "@headlessui/react";
+import { makeClassName, tremorTwMerge } from "lib";
+import { RootStylesContext } from "contexts";
+
+const makeDisplayClassName = makeClassName("dialog");
+
+export type DialogPanelProps = React.HTMLAttributes<HTMLDivElement>;
+
+const DialogPanel = React.forwardRef<HTMLDivElement, DialogPanelProps>((props, ref) => {
+  const { children, className, ...other } = props;
+  const rootStyles =
+    React.useContext(RootStylesContext) ?? tremorTwMerge("rounded-tremor-default p-6");
+
+  return (
+    <Transition.Child
+      as={React.Fragment}
+      enter="ease-out duration-300"
+      enterFrom="opacity-0 scale-95"
+      enterTo="opacity-100 scale-100"
+      leave="ease-in duration-200"
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
+    >
+      <HeadlessuiDialog.Panel
+        ref={ref}
+        className={tremorTwMerge(
+          makeDisplayClassName("panel"),
+          // light
+          "bg-tremor-background  text-tremor-content ring-tremor-ring",
+          // dark
+          "dark:bg-dark-tremor-background dark:text-dark-tremor-content dark:ring-dark-tremor-ring",
+          // common
+          "max-w-lg overflow-hidden text-left shadow-xl ring-1 shadow-tremor transition-all transform",
+          rootStyles,
+          className,
+        )}
+        {...other}
+      >
+        {children}
+      </HeadlessuiDialog.Panel>
+    </Transition.Child>
+  );
+});
+
+DialogPanel.displayName = "DialogPanel";
+
+export default DialogPanel;
