c8582efea093e8e5209e3768e0fb2135d4550511
severinlandolt <sev.landolt@gmail.com>
2024-12-12 17:39:45 +0100

    fix: accordion exports

--- src/components/layout-elements/Accordion/index.ts
+++ src/components/layout-elements/Accordion/index.ts
@@ -1,4 +1,6 @@
 export { Accordion } from "./Accordion";
 export type { AccordionProps } from "./Accordion";
+export { AccordionBody } from "./AccordionBody";
+export { AccordionHeader } from "./AccordionHeader";
 export { AccordionList } from "./AccordionList";
 export type { AccordionListProps } from "./AccordionList";
